e6c1a2792a8b164b2044a98baca692711ccee4b6
bilal <bill.ahmed@mail.utoronto.ca>
2021-11-18 20:56:49 -0500

    render invoice based on mock data - able to print to pdf

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="col h-screen">
+    <div class="shadow-md p-2 bg-white"> Header </div>
+
+    <div class="row w-screen flex-grow overflow-x-auto">
+      <div class="debug shadow-md m-4 p-2 w-1/6 bg-white rounded-lg">
+        Left panel
+
+        <button @click="print">print </button>
+      </div>
+      <EditorVue :meta="invoiceMeta" :data="invoiceData"/>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import EditorVue from './components/Editor.vue'
+import mockData from './mockData.json'
+import editorStyles from './styles/app.css.json';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    EditorVue
+  },
+  data() {
+    return {
+      invoiceMeta: {
+        id: '000004',
+        dateOfIssue: '11-11-2021',
+        dueDate: '11-12-2021',
+        currency: 'CAD',
+        tax: 0,
+        amountPaid: 0
+      },
+      invoiceData: mockData.invoiceData,
+    }
+  },
+  methods: {
+    print() {
+      // Element to print
+      let elem = document.getElementById('printMe')
+
+      // Grab any scoped styles we might need
+      let stylesToAdd = editorStyles.styles.reduce((p, c) => { return p + '\n' + c[0] + c[1] }, '')
+
+      var printWindow = window.open('', '', 'height=600,width=900');
+      printWindow.document.write(
+        `
+          <html>
+            <head>
+              <title>Invoice ${this.invoiceMeta.id}</title>
+              <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+            </head>
+
+            <body>
+              ${elem.innerHTML}
+            </body>
+            <style>
+              ${stylesToAdd}
+            </style>
+          </html>
+        `
+      );
+
+      // Need to wait for the styles to load
+      // If css doesn't load fast enough, 
+      // may need to bump the timeout...        
+      setTimeout(() => {
+        printWindow.print();
+        printWindow.close();
+      }, 500)
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/Editor.vue
@@ -0,0 +1,149 @@
+<template>
+  <div id="printMe" style="width: 8.5in" class=" editor-container shadow-xl my-4 mx-auto p-16 bg-white overflow-y-auto">
+    <!-- From address -->
+    <div class="mb-10 whitespace-pre">
+      <p>
+        {{ data.header.from.name }}
+      </p>
+      <p>
+        {{ data.header.from.address }}
+      </p>
+    </div>
+
+    <!-- To address + Summary -->
+    <div class="flex flex-row justify-between">
+      <div class="whitespace-pre">
+        <p>
+        {{ data.header.to.name }}
+        </p>
+        <p>
+          {{ data.header.to.address }}
+        </p>
+      </div>
+
+      <!-- Invoice summary -->
+      <div class="flex flex-col blue-color font-semibold">
+        <div class="flex flex-row justify-between">
+          <p> Invoice Number </p>
+          <p> {{meta.id}} </p>
+        </div>
+
+        <div class="flex flex-row justify-between">
+          <p> Date of Issue </p>
+          <p> {{meta.dateOfIssue}} </p>
+        </div>
+
+        <div class="flex flex-row justify-between">
+          <p> Due Date </p>
+          <p> {{meta.dueDate}} </p>
+        </div>
+
+        <div class="flex flex-row justify-between">
+          <p> Amount Due ({{meta.currency}}) &nbsp; </p>
+          <p> ${{amountDue}} </p>
+        </div>
+      </div>
+    </div>
+
+    <hr class="my-2 mt-8 border-t-4 blue-color"/>
+
+    <!-- Main body -->
+    <div class="mb-12">
+      <table class="w-full">
+        <thead class="text-right blue-color">
+          <th class="text-left"> Description </th>
+          <th class="pr-4"> Rate </th>
+          <th class="pr-4"> Qty </th>
+          <th> Line Total </th>
+        </thead>
+        
+        <div class="mb-1"/>
+
+        <tbody>
+          <tr 
+            v-for="(item, index) in data.body.items" 
+            :key="index"
+            class="text-right" style="border-bottom: solid 1px #E5E5E5"
+          > 
+            <td class="py-3 pr-24 description text-left text-sm"> {{item.description}} </td>
+            <td class="py-3 pr-4 text-sm min-w-16"> ${{item.rate}} </td>
+            <td class="py-3 pr-4 text-sm min-w-16"> {{item.qty}} </td>
+            <td class="py-3 text-sm min-w-16"> ${{item.qty * item.rate}} </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+
+    <!-- Subtotals -->
+    <div class="flex flex-row justify-end mb-12">
+      <div class="flex flex-col">
+        <div class="flex flex-row justify-between"> 
+          <p> Subtotal </p> 
+          <p> {{amountDue}} </p>
+        </div>
+        
+        <div class="flex flex-row justify-between">
+          <p> Tax </p>
+          <p> {{meta.tax}} </p>
+        </div>
+
+        <hr class="my-2"/>
+
+        <div class="flex flex-row justify-between">
+          <p>Total</p>
+          <p> {{totalWithTax}} </p>
+        </div>
+
+        <div class="flex flex-row justify-between">
+          <p> Amount Paid </p>
+          <p> {{meta.amountPaid}} </p>
+        </div>
+
+        <hr class="my-4"/>
+
+        <div class="flex flex-row justify-between w-72">
+          <p class="blue-color font-semibold"> Amount Due ({{meta.currency}}) &nbsp; </p>
+          <p> {{finalTotal}} </p>
+        </div>
+      </div>
+    </div>
+
+    <!-- Notes -->
+    <div>
+      <label class="blue-color font-semibold">Notes</label>
+      <p> {{data.footer.notes}} </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['meta', 'data'],
+  data() {
+    return { }
+  },
+  computed: {
+    amountDue() {
+      return this.data.body.items.reduce((p, c) => { return p + (c.rate * c.qty) }, 0)
+    },
+    totalWithTax() {
+      return this.amountDue * (1 + this.meta.tax);
+    },
+    finalTotal() {
+      return this.totalWithTax - this.meta.amountPaid;
+    }
+  }
+}
+</script>
+
+<style>
+/* 
+* ANY AND ALL STYLES HERE MUST ALSO 
+* BE INCLUDED IN `app.css.json`
+*
+* Try to keep these at a minimum...
+*/
+
+.blue-color { color: #4f697a; border-color: #4f697a; }
+
+</style>
